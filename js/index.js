fa7da6e6a64fae1155986942cd691aafe57e9d5a
Able-Fan <842615663@qq.com>
2016-12-06 08:14:51 +0800

    add

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(function(){
+    var langth=0;
+    var height1 = parseInt($(".fullpage").css("height"));
+    console.log($("body").css("height"));
+    var fullpage = document.querySelector(".fullpage");
+    console.log(fullpage.offsetHeight);
+    touch.on("body","swipeup",".fullpage",function(){
+        langth+=100;
+        if(langth>1100){
+            langth = height1;
+        }
+        $(".fullpage").css({
+            marginTop:-langth
+        })
+        console.log(langth);
+    });
+    touch.on("body","swipedown",".fullpage",function(){
+        langth-=100;
+        if(langth<=0){
+            langth = 0;
+        }
+        $(".fullpage").css({
+            marginTop:-langth
+        })
+    })
+});
